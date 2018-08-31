6c7332b511c4c9dcf96d6369076ab695bdf47359
sapsley <spencer.apsley@gmail.com>
2018-08-31 12:51:10 -0600

    Added client for making requests directly to the API

---
+++ NokeMobileLibrary/Classes/NokeLibraryApiClient.swift
@@ -0,0 +1,17 @@
+//
+//  NokeLibraryApiClient.swift
+//  NokeMobileLibrary
+//
+//  Created by Spencer Apsley on 8/31/18.
+//
+
+import Foundation
+
+
+struct API{
+    
+    //Endpoints
+    static let UPLOAD               = "upload/"
+    static let RESTORE              = "restore/"
+    static let CONFIRM_RESTORE      = "restore/confirm/"
+}
